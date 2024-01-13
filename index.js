22c1c7e18bc4b2d442f586adc9abadcc96e38dfb
Shivam Kumar <satishshivam697@gmail.com>
2024-01-14 02:53:28 +0530

    Backend With SQL data Model and Upadted QR feature in Node Only Need to Test

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const routes = require('./routes/routes');
+
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Moye Moye');
-});
+// QR Routes
+// Hotel portal route
+app.get('/hotel', (req, res) => {
+    res.send('Welcome to the Hotel Portal! Please enter your Room Number.');
+  });
+  
+  // Cafe portal route
+  app.get('/cafe', (req, res) => {
+    res.send('Welcome to the Cafe Portal! Please enter your Table Number.');
+  });
+  
+  // Restaurant portal route
+  app.get('/restaurant', (req, res) => {
+    res.send('Welcome to the Restaurant Portal! Please enter your Table Number.');
+  });
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Use the routes defined in routes.js
+app.use('/', routes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
