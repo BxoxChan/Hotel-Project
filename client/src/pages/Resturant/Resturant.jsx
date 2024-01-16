50bb52f4d858192b199067bcdf060f0701f694c8
1234bhaskar <bhaskar2021chand@gmail.com>
2024-01-16 19:45:49 +0530

    Resturant Page Created

---
+++ client/src/pages/Resturant/Resturant.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import SearchBar from '../../components/SearchBar'
+import MenuComp from '../../components/MenuComp'
+import { NavLink } from 'react-router-dom'
+
+export default function Resturant() {
+  return (
+    <div className='bg-resturant bg-cover bg-no-repeat relative min-h-screen '>
+     <header className='text-center text-black  font-Ubuntu text-3xl py-5 bg-orangeD1'>MEJBAN  <span className='text-4xl font-bold text-black'>Resturant</span></header>
+       <SearchBar/>
+        <MenuComp/>
+        {/* place order */}
+
+        <NavLink to={"/cafe/confirmOrderCafe"} className ="bg-orange-500 text-white w-full  absolute bottom-0 py-2 text-2xl text-center">Cart</NavLink>
+    
+    </div>
+  )
+}
