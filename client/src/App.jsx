30c898933a00262336b26eda3805909e8f6531c7
1234bhaskar <bhaskar2021chand@gmail.com>
2024-01-17 18:08:42 +0530

    2 new Route creates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import Cafe from './pages/Cafe'
 import Resturant from './pages/Resturant/Resturant'
 import Hotel from './pages/Hotel/Hotel'
 import CafeConfirm from './pages/CafeConfirm'
+import HotelServices from './pages/Hotel/HotelServices'
+import OrderFood from './pages/Hotel/OrderFood'
 
 export default function App() {
   return (
@@ -16,6 +18,8 @@ export default function App() {
             <Route path='/cafe/confirmOrderCafe' element={<CafeConfirm/>}/>
             <Route path='/resturant' element={<Resturant/>}/>
             <Route path='/hotel' element={<Hotel/>}/>
+            <Route path='/hotel/orderFood' element={<OrderFood/>}/>
+            <Route path='/hotel/hotelServices' element={<HotelServices/>}/>
         </Routes>
         </Router>
     </div>
