d81a2346a439432938375065d10e9427607b4c6d
1234bhaskar <bhaskar2021chand@gmail.com>
2024-01-31 21:26:26 +0530

    Created Comp OrdersMainComp

--- client/src/components/Admin/OrdersComp/OrdersMainComp.jsx
+++ client/src/components/Admin/OrdersComp/OrdersMainComp.jsx
@@ -14,7 +14,6 @@ export default function OrdersMainComp() {
         <button className={activeComponent==='Resturant'?`rounded-sm border-2 w-1/5 border-black bg-orangeD1 text-white p-2 font-bold`:`rounded-md border-2 w-1/5 border-black bg-orange-400 text-white ` } onClick={() => handleButtonClick('Resturant')}>Resturant</button>
         <button className={activeComponent==='Hotel'?`rounded-sm border-2 w-1/5 border-black bg-orangeD1 text-white p-2 font-bold`:`rounded-md border-2 w-1/5 border-black bg-orange-400 text-white ` } onClick={() => handleButtonClick('Hotel')}>Hotel</button>
        </div>
-
        <div className='border-black border-2 h-4/5 w-full  overflow-y-scroll absolute bottom-0 '>
          <OrdersTable comp={activeComponent}/>
        </div>
