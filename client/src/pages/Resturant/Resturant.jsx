90808490735a18a4648c96178cec123c2ef90aac
1234bhaskar <bhaskar2021chand@gmail.com>
2024-02-02 13:11:17 +0530

    Ui improved

--- client/src/pages/Resturant/Resturant.jsx
+++ client/src/pages/Resturant/Resturant.jsx
@@ -5,13 +5,18 @@ import { NavLink } from 'react-router-dom'
 
 export default function Resturant() {
   return (
-    <div className='bg-resturant bg-cover bg-no-repeat relative min-h-screen '>
+    <div className='bg-resturant bg-cover bg-no-repeat relative h-screen '>
      <header className='text-center text-black  font-Ubuntu text-3xl py-5 bg-orangeD1'>MEJBAN  <span className='text-4xl font-bold text-black'>Resturant</span></header>
-       <SearchBar/>
+       {/* <SearchBar/>
+        <MenuComp/> */}
+
+         <div className="h-85% border-2">
+        <SearchBar/>
         <MenuComp/>
+        </div>
         {/* place order */}
 
-        <NavLink to={"/cafe/confirmOrderCafe"} className ="bg-orange-500 text-white w-full  absolute bottom-0 py-2 text-2xl text-center">Cart</NavLink>
+        <NavLink to={"/cafe/confirmOrderCafe"} className ="bg-orange-500 text-white w-full absolute bottom-0 py-2 text-2xl text-center">Cart</NavLink>
     
     </div>
   )
