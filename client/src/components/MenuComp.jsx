c1258cd08d2b1d47a7729e0ecdd30d3bfdebe80d
1234bhaskar <bhaskar2021chand@gmail.com>
2024-02-02 13:10:49 +0530

    UI updated

--- client/src/components/MenuComp.jsx
+++ client/src/components/MenuComp.jsx
@@ -3,7 +3,13 @@ import DishCard from './DishCard'
 
 export default function MenuComp() {
   return (
-     <div className='h-menu border border-black  mx-2 p-2 overflow-y-scroll backdrop-brightness-75 backdrop-blur-sm'>
+     <div className='h-90% border border-black  mx-2 p-2 overflow-y-scroll backdrop-brightness-75 backdrop-blur-sm'>
+      <DishCard/>
+      <DishCard/>
+      <DishCard/>
+      <DishCard/>
+      <DishCard/>
+      <DishCard/>
       <DishCard/>
       <DishCard/>
       <DishCard/>
