566a3f0cd34fef8b64c0e5220ae126e7d0a7cba4
1234bhaskar <bhaskar2021chand@gmail.com>
2024-02-09 19:22:22 +0530

    Added Customer Details functionality

--- client/src/pages/Cafe.jsx
+++ client/src/pages/Cafe.jsx
@@ -1,9 +1,76 @@
 import { NavLink } from "react-router-dom";
 import MenuComp from "../components/MenuComp";
 import SearchBar from "../components/SearchBar";
+import { useEffect, useState } from "react";
+import TableNO from "../components/TableNO";
 //import bg from "../../public/cafeBG.jpg"
 
+
 export default function Cafe() {
+  const [showTable,setshowTable]=useState(false);
+
+  const [tableDetails, setTableDetails] = useState({
+    table: '',
+    phone: '',
+    name: '',
+  });
+
+
+  // const [tableDetails,setTableDetails]=useState(
+  //    {
+  //    table:'',
+  //    phone:'',
+  //    name:''
+  //  }
+  // )
+  
+  // const [userDetails,setUserDetails] = useState({
+  //   name:'',
+  //   phone:'',
+  //   table:'',
+  // })
+  // const [tableNo,setTableNo]=useState('');
+  // const [phone,setPhone]=useState('');
+  // const [name,setName]=useState('');
+// const [tableDetails,setTableDetails]=useState(
+//      {
+//      table:tableNo,
+//      phone:phone,
+//      name:name
+//    }
+//   )
+  
+
+  const handleTableDetails=()=>{
+    setshowTable(true);
+  }
+
+
+  // const confirmDetails = ()=>{
+  //   // console.log(name);
+  //   // console.log(tableNo);
+  //   // console.log(phone);
+  //   setshowTable(false);
+  //   // console.log(name);
+  //   // console.log(tableNo);
+  //   // console.log(phone);
+  // //   setTableDetails({
+  // //    table:tableNo,
+  // //    phone:phone,
+  // //    name:name
+  // //  })
+  //   console.log(tableDetails);
+  //   // console.log(name);
+  //   // console.log(tableNo);
+  //   // console.log(phone);
+  // }
+  
+  const handleSubmit = (formData) => {
+        setshowTable(false);
+    // Do something with the form data
+    console.log(formData);
+  };
+
   return (
     <div className="bg-jails h-screen bg-opacity bg-center bg-no-repeat bg-cover relative ">
         <header className='text-center bg-black  font-Ubuntu text-3xl py-5 text-orangeD1'>MEJBAN <span className="text-white text-4xl font-bold ">Jail</span> <span className='text-4xl font-bold text-orangeD1'>Cafe</span></header>
@@ -12,8 +79,21 @@ export default function Cafe() {
         <MenuComp/>
         </div>
         {/* place order */}
+         {
+          (tableDetails.table !='' && tableDetails.phone !='' && tableDetails.name !='')?
+            <NavLink  to={"/cafe/confirmOrderCafe"} className ="bg-orange-500 text-white w-full absolute bottom-0 py-2 text-2xl text-center">Cart</NavLink>
+          :
+          <button className="bg-orange-500 text-white w-full absolute bottom-0 py-2 text-2xl text-center" onClick={handleTableDetails}>Select Table</button>
+         }
 
-        <NavLink to={"/cafe/confirmOrderCafe"} className ="bg-orange-500 text-white w-full absolute bottom-0 py-2 text-2xl text-center">Cart</NavLink>
+
+         {
+          showTable && 
+          <div className="absolute top-0 w-full  border-2 border-orangeD1 h-3/4 flex items-center justify-center backdrop-brightness-50">
+            <TableNO onSubmit={handleSubmit}   setTableDetails={setTableDetails} tableDetails={tableDetails}/>
+          </div>
+          
+         }
     </div>
   )
 }
