37cb98862d3a4721be17c5c2ccbf3d416357492b
1234bhaskar <bhaskar2021chand@gmail.com>
2024-02-10 15:47:23 +0530

    Customized according to Hotel/Res and Cafe

--- client/src/components/TableNO.jsx
+++ client/src/components/TableNO.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 
-export default function TableNO({setTableDetails,tableDetails,onSubmit}) {
+export default function TableNO({setTableDetails,tableDetails,onSubmit,hotel}) {
   const handleChange = (e) => {
     const { name, value } = e.target;
     setTableDetails(prevState => ({
@@ -16,10 +16,10 @@ export default function TableNO({setTableDetails,tableDetails,onSubmit}) {
   };
     return (
     <form className='bg-white w-full mx-5  rounded-sm py-5 flex flex-col items-center font-Ubuntu' onSubmit={handleSubmit}>
-        <header className='text-center text-2xl mb-2 font-bold font-serif'>Table. Details</header>
+        <header className='text-center text-2xl mb-2 font-bold font-serif'>{hotel?"Room Details":"Table Details"}</header>
         <div>
-        <h1>Please Enter Your Table No.</h1>
-        <input type="number" name='table' value={tableDetails.table} onChange={handleChange} className='border p-2 border-orange-400 ' placeholder='Tabel no.'/>
+        <h1>Please Enter Your {hotel?"Room":"Table"} No.</h1>
+        <input type="number" name='table' value={tableDetails.table} onChange={handleChange} className='border p-2 border-orange-400 ' placeholder='Tabel no./Room No.'/>
         <h1>Enter Your Name</h1>
         <input type="text" name='name' value={tableDetails.name} onChange={handleChange} className='border p-2 border-orange-400' placeholder='Mr./Mrs.'/>
         <h1>Enter your Phone no.</h1>
