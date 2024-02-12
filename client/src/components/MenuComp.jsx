0f120f5815c5f0cef84846030221734ee0836b7e
1234bhaskar <bhaskar2021chand@gmail.com>
2024-02-12 07:48:09 +0530

    cleaning unnecesary things

--- client/src/components/MenuComp.jsx
+++ client/src/components/MenuComp.jsx
@@ -21,10 +21,31 @@ export default function MenuComp() {
     fetchMenuItems();
   }, []);
 
-  const addToCart = (menuItem) => {
-    setCartItems([...cartItems, menuItem]);
-    console.log('Item added to cart:', menuItem);
-  };
+  // const addToCart = (menuItem) => {
+     
+  //    //const updateCart=cartItems.push(menuItem)
+  //   //  if(cartItems.length==0){
+  //   //         const updateCart=cartItems.push(menuItem)
+  //   //         setCartItems(updateCart)
+  //   //         console.log("ADDED 0");
+  //   //  }
+     
+  //     //const updateCart=cartItems.push(menuItem)
+  //      // setCartItems(updateCart)
+  //      setCartItems(prev => [...prev,menuItem]);
+     
+  //     // console.log(cartItems.length);
+  //      //setCartItems([...cartItems, menuItem]);
+  //      console.log('Item added to cart:', cartItems.map((a)=>(a)));
+  //  // console.log('Cart Items:',cartItems);
+
+  //    //  if(cartItems.length<1){
+  // //    setCartItems(cartItems.push(menuItem))
+  // //  }else{
+  // //   //console.log([...cartItems,menuItem]);
+  // //    setCartItems(...cartItems,menuItem);
+  // //  }
+  // };
   
   if (menuItems.length === 0 && !error) {
     return <div>Loading...</div>;
@@ -37,7 +58,7 @@ export default function MenuComp() {
   return (
     <div className='h-3/4 border border-black mx-2 p-2 overflow-y-scroll backdrop-brightness-75 backdrop-blur-sm'>
       {menuItems.map((menuItem) => (
-        <DishCard key={menuItem.item_id} menuItem={menuItem} addToCart={addToCart} />
+        <DishCard key={menuItem.item_id} menuItem={menuItem}  cartItems={cartItems} setCartItems={setCartItems} />
       ))}
     </div>
   );
