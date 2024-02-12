2bfd1f2e0c893e8a8caea37363cde9ae8079bdcd
1234bhaskar <bhaskar2021chand@gmail.com>
2024-02-12 07:46:40 +0530

    structural changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter as Router} from  'react-router-dom'
+import { ContextProvider } from './context/OrderContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Router>
+      <ContextProvider>
+
     <App />
+      </ContextProvider>
+    </Router>
   </React.StrictMode>,
 )
