662605b4152e901ca0210348fec775ecfaeb522b
1234bhaskar <bhaskar2021chand@gmail.com>
2024-02-13 14:54:17 +0530

    adding order in Cart context which includes everything about Order

--- client/src/context/OrderContext.jsx
+++ client/src/context/OrderContext.jsx
@@ -5,8 +5,9 @@ export const Cart = createContext({});
 export const ContextProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
   const [table, setTable] = useState([]);
+  const [order,setOrder]=useState([]);
 
-  return (<Cart.Provider value={{ cart, setCart,table,setTable }}>{children}</Cart.Provider>);
+  return (<Cart.Provider value={{ cart, setCart,table,setTable,order,setOrder }}>{children}</Cart.Provider>);
 };
 
 export default ContextProvider;
