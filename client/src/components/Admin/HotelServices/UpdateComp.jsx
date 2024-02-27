92b650168f84aaef3f85b02bde84c62b6fd89975
1234bhaskar <bhaskar2021chand@gmail.com>
2024-02-27 13:53:24 +0530

    Ui updt(mobile Responsive) admin/HotelService

--- client/src/components/Admin/HotelServices/UpdateComp.jsx
+++ client/src/components/Admin/HotelServices/UpdateComp.jsx
@@ -4,14 +4,14 @@ export default function UpdateComp() {
   return (
     <div className='bg-white w-4/5 h-4/5 rounded-md drop-shadow-md p-2'>
           <h1 className='text-xl font-Raleway underline'>Add a new Service</h1>
-          <form className='h-4/5 border-2 flex justify-around items-center'>
+          <form className='h-4/5 border-2 sm:flex justify-around items-center pt-5 pl-5 sm:pl-0 sm:pt-0'>
             {/* <input type="file" title='image' name="filename"/> */}
             <div className=''>
             <label htmlFor="" className='font-bold'>Name:</label>
             <input type='input' placeholder='Name of Service' className='border'/>
             </div>
             
-            <button className='bg-orangeD1 text-white py-2 px-5 rounded-lg'>Add</button>
+            <button className='bg-orangeD1 text-white py-2 px-5 rounded-lg mt-2 sm:mt-0'>Add</button>
             
           </form>
     </div>
--- client/src/pages/Admin/HotelServicesUpdate.jsx
+++ client/src/pages/Admin/HotelServicesUpdate.jsx
@@ -6,9 +6,9 @@ import HotelServicesList from '../../components/Admin/HotelServices/HotelService
 export default function HotelServicesUpdate() {
   return (
     <div className='mx-0 px-0 box-border h-screen sm:flex  sm:border-black sm:h-screen'>
-         <div className='w-1/5 shadow-shadowCust z-10'><SideBarMenu/></div>
+         <div className='sm:w-1/5 shadow-shadowCust z-10'><SideBarMenu/></div>
          
-            <div className='w-4/5 bg-Cust1 h-screen'>
+            <div className='sm:w-4/5 bg-Cust1 h-screen'>
           <div className='h-2/5 border-2 flex justify-center items-center'>
             <UpdateComp/>
           </div>
