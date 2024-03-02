2337f037910d8852bccd88c32ac544891f1cb1ab
Shivam Kumar <satishshivam697@gmail.com>
2024-03-02 20:10:30 +0530

    DataPool is created so that databse will not stop after some time

--- routes/db.js
+++ routes/db.js
@@ -1,19 +1,23 @@
 const mysql = require('mysql2');
 
 // Database connection setup
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: '34.71.38.114',
   user: 'admin',
   password: '123456789',
   database: 'hotel',
+  waitForConnections: true, 
+  connectionLimit: 10, 
+  queueLimit: 0 
 });
 
-db.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
-    console.error('Database connection error:', err);
+    console.error('Error getting database connection from the pool:', err);
   } else {
     console.log('Connected to the database');
+    connection.release(); 
   }
 });
 
-module.exports = db;
+module.exports = pool;
